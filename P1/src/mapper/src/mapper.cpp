9c8f84f3e28586c7ab91021f532c92f408932969
MLB51 <mlb51@gcloud.ua.es>
2024-03-13 20:35:54 +0100

    progreso en alg

--- P1/src/mapper/src/mapper.cpp
+++ P1/src/mapper/src/mapper.cpp
@@ -22,7 +22,13 @@
 
 #include <algorithm>
 
+#include <Eigen/Dense>
+
 namespace fs = boost::filesystem;
+
+const int ICP_MAX_ITER=50;
+
+
 
 /*
 Feature extraction: 
@@ -42,13 +48,13 @@ Descriptor Extraction:
 
 /*
 Nubes problematicas (?)
-    R0.1 ->
-    R0.5 ->
-    R1 -> 0, 2, 16, 17, 18, 46, 83, 85, 86 
+    R0.1 -> 0,2, 4, 5, 14, 15, 16, 17, 18, 21, 23, 24 27, 29, 35 ,41,42,44,45,46, 49, 52,53, 55, 60,79,80,81,83,84,85,86,87, 88, 90
+    R0.5 -> 
+    R1 -> 0, 2, 16, 17, 18, 46, 83, 85, 86
 */
-void extractDescriptors(
+void extractKeypoints(
         pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, 
-        pcl::PointCloud<pcl::PointXYZI>::Ptr keypoints, 
+        pcl::PointCloud<pcl::PointXYZI>::Ptr keypoints,
         pcl::PointCloud<pcl::FPFHSignature33>::Ptr descriptors
     ){
     pcl::PointCloud<pcl::Normal>::Ptr normals(new pcl::PointCloud<pcl::Normal>);
@@ -65,7 +71,7 @@ void extractDescriptors(
     harris.setInputCloud(cloud);
     harris.setNormals(normals);
     harris.setNonMaxSupression(true); // pasar a TRUE si se encuentran demasiados
-    harris.setRadius(0.5); // default 0.1, determina el tamaño de los descriptores
+    harris.setRadius(0.1); // default 0.1, determina el tamaño de los descriptores
     harris.setThreshold(0.01); // default 0.01, menor thr == mas kps
     harris.compute(*keypoints);
 
@@ -81,66 +87,98 @@ void extractDescriptors(
     fpfh.compute(*descriptors);
 }
 
+Eigen::Matrix4f pairKeypoints(
+        pcl::PointCloud<pcl::PointXYZI>::Ptr keypoints,
+        pcl::PointCloud<pcl::FPFHSignature33>::Ptr descriptors,
+        pcl::PointCloud<pcl::PointXYZI>::Ptr previous_keypoints,
+        pcl::PointCloud<pcl::FPFHSignature33>::Ptr previous_descriptors
+    ){
+    pcl::Registration<PointT, PointT>::Ptr registration(new pcl::Registration<PointT, PointT>);
+
+    // Estimate correspondences between keypoints based on descriptors
+    pcl::registration::CorrespondenceEstimation<PointT, PointT>::Ptr correspondence_estimation(new pcl::registration::CorrespondenceEstimation<PointT, PointT>);
+    correspondence_estimation->setInputSource(keypoints);
+    correspondence_estimation->setInputTarget(previous_keypoints);
+    correspondence_estimation->setInputCorrespondences(descriptors, previous_descriptors);
+
+    // Apply RANSAC to estimate the transformation
+    pcl::registration::CorrespondenceRejectorSampleConsensus<PointT>::Ptr ransac(new pcl::registration::CorrespondenceRejectorSampleConsensus<PointT>);
+    ransac->setInputSource(keypoints);
+    ransac->setInputTarget(previous_keypoints);
+    ransac->setInlierThreshold(0.05); // Adjust this threshold based on your application
+    ransac->setMaximumIterations(1000); // Adjust maximum iterations as needed
+    ransac->setInputCorrespondences(correspondence_estimation->getCorrespondences());
+    ransac->setInputTransformation(registration->getInputTransformation());
+
+    // Perform registration
+    pcl::registration::TransformationEstimation<PointT, PointT>::Ptr transformation_estimation(new pcl::registration::TransformationEstimation<PointT, PointT>);
+    transformation_estimation->estimateRigidTransformation(*keypoints, *previous_keypoints, *ransac->getInliers(), registration->getTransformation());
+
+    // Get the final transformation
+    return registration->getTransformation();
+}
 
 void pc2map(std::string path){
 
-    // 1: Transformación total T T = I
-    // 2: Mapa 3D M
-    // 3: para i=1 hasta t-1 hacer
-
+    pcl::PointCloud<pcl::PointXYZI>::Ptr previous_kps(new pcl::PointCloud<pcl::PointXYZI>);
+    pcl::PointCloud<pcl::FPFHSignature33>::Ptr previous_dscs(new pcl::PointCloud<pcl::FPFHSignature33>);
+    
+    Eigen::Matrix3d total_transformation = Eigen::Matrix3d::Identity();
+    
+    pcl::PointCloud<pcl::PointXYZ>::Ptr map(new pcl::PointCloud<pcl::PointXYZ>);
+    
+    // gets all the files paths
     std::vector<fs::path> files;
     for (const auto& entry : fs::directory_iterator(path)) {
         files.push_back(entry.path());
     }
-    // sorts the files alphabetically
+    // sorts the files alphabetically as they are saved like cloud_<i>
     std::sort(files.begin(), files.end());
 
     int i=0;
     for (const auto& entry : files) {
-        std::cout << entry << std::endl;
+        std::cout << std::endl << entry << std::endl;
         pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
 
 
         if (pcl::io::loadPCDFile<pcl::PointXYZ> (entry.string(), *cloud) == -1){
-            PCL_ERROR ("Couldn't read file test_pcd.pcd \n");
+            PCL_ERROR ("Couldn't read file "+entry.string()+"\n");
             return;
         }
 
-        std::cout << "Loaded "
-                    << cloud->width * cloud->height
-                    << " data points from test_pcd.pcd with the following fields: "
-                    << std::endl;
-
-
-        pcl::PointCloud<pcl::PointXYZI>::Ptr keypoints(new pcl::PointCloud<pcl::PointXYZI>);
-        pcl::PointCloud<pcl::FPFHSignature33>::Ptr descriptors(new pcl::PointCloud<pcl::FPFHSignature33>);
-        extractDescriptors(cloud, keypoints, descriptors);
-
+        std::cout << "Loaded cloud with " << cloud->width * cloud->height << " data points" <<std::endl;
+        
+        // keypoints are extracted
+        pcl::PointCloud<pcl::PointXYZI>::Ptr kps(new pcl::PointCloud<pcl::PointXYZI>);
+        pcl::PointCloud<pcl::FPFHSignature33>::Ptr dscs(new pcl::PointCloud<pcl::FPFHSignature33>);
+        extractKeypoints(cloud, kps, dscs);
         std::cout << "Number of keypoints: " << keypoints->size() << std::endl;
         std::cout << "Number of descriptors: " << descriptors->size() << std::endl;
 
+        if(i>0){
+            std::cout << "Pairing... " << std::endl;
+
+            // the i matrix is calculated on pairKeypoints and then multiplied by the total
+            total_transformation = total_transformation * pairKeypoints(kps, dscs, previous_kps, previous_dscs);
+
+            // the total is applied to the whole cloud
+            pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_transformed(new pcl::PointCloud<pcl::PointXYZ>);
+            pcl::transformPointCloud(*cloud, *cloud_transformed, total_transformation);
+
+            // the transformed cloud is added to the map
+            *map += cloud_transformed; 
+            
+        }else{
+
+            std::cout << "1st iter, pairing can't be done." << std::endl;
+        }
+        // por ultimo se copian los kps actuales para la siguiente iter
+        *previous_kps = *kps;
+        *previous_dscs = *dscs;
+
         i++;
     }
-        // 4:
-        // Extraer características C i = {c i 1 , c i 2 , ..., c im } de los datos x i .
-        // i+1
-        // i+1
-        // 5:
-        // Extraer características C i+1 = {c i+1
-        // 1 , c 2 , ..., c k } de los datos x i+1 .
-        // 6:
-        // Encontrar emparejamientos P = {p 1 , p 2 , ..., p l } entre las características
-        // C i y C i+1 . Cada emparejamiento es un par de características de cada
-        // conjunto de datos p j = {c ia , c i+1
-        // b }
-        // 7:
-        // Obtener la mejor transformación T i que explican los emparejamientos.
-        // 8:
-        // Obtener la transformación total T T = T T ∗ T i .
-        // 9:
-        // Aplicar al conjunto de datos C i+1 la transformación T T para colocar-
-        // los en el sistema de coordenadas de C 1 . Acumular el conjunto de datos
-        // transformados en M .
+        
     // 10: fin para
     // 11: devolver M
 
